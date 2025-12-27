27a4c89d1c2b05a5b4b21673c72b3ae6c587df59
Yash Padhiyar <yashpadhiyar.pyr@gmail.com>
2025-12-27 16:32:01 +0530

    change in TestOrderDto

--- src/main/java/com/example/pathology/Services/TestOrderService.java
+++ src/main/java/com/example/pathology/Services/TestOrderService.java
@@ -1,8 +1,11 @@
 package com.example.pathology.Services;
 
+import com.example.pathology.entities.Test;
 import com.example.pathology.entities.TestOrder;
 import com.example.pathology.entities.TestOrderDto;
 import com.example.pathology.repos.TestOrderRepo;
+import com.example.pathology.repos.TestRepo;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ import java.util.UUID;
 public class TestOrderService {
 
     private final TestOrderRepo testOrderRepo;
+    private final TestRepo testRepo;
 
     public TestOrder getOrderById(UUID id){
         return testOrderRepo.getReferenceById(id);
@@ -23,10 +27,15 @@ public class TestOrderService {
         return testOrderRepo.findAll();
     }
 
+    @Transactional
     public TestOrder createOrder(TestOrderDto testOrderDto){
+
+        Test test = testRepo.getReferenceById(testOrderDto.getSelectedTest());
+
         TestOrder testOrder = TestOrder.builder()
                 .patientName(testOrderDto.getPatientName())
                 .phoneNumber(testOrderDto.getPhoneNumber())
+                .selectedTest(test)
                 .build();
 
         return testOrderRepo.save(testOrder);
--- src/main/java/com/example/pathology/entities/TestOrderDto.java
+++ src/main/java/com/example/pathology/entities/TestOrderDto.java
@@ -14,4 +14,5 @@ import java.util.UUID;
 public class TestOrderDto {
     private String patientName;
     private Integer phoneNumber;
+    private UUID selectedTest;
 }
